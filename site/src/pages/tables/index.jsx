12d11eb57a68cc8b13570552d65430e99d84466c
aurelianbasa <56235624+aurelianbasa@users.noreply.github.com>
2023-03-23 19:15:57 -0400

    Update index.jsx

--- site/src/pages/tables/index.jsx
+++ site/src/pages/tables/index.jsx
@@ -26,6 +26,17 @@ const Tables = props => {
       <Helmet>
         <style type='text/css'>
           {`
+          body {
+            font-family: sans-serif;
+            -webkit-font-smoothing: auto;
+            -moz-font-smoothing: auto;
+            -moz-osx-font-smoothing: grayscale;
+            font-smoothing: auto;
+            text-rendering: optimizeLegibility;
+            font-smooth: always;
+            -webkit-tap-highlight-color: transparent;
+            -webkit-touch-callout: none;
+            }
 						table {
 							width:100%;
 							border:1px solid #606060;
