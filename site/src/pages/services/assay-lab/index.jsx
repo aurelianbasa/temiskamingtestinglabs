8ce1dab151e00971162ea159435d5f0394bf6873
aurelianbasa <56235624+aurelianbasa@users.noreply.github.com>
2023-06-12 12:23:23 -0400

    Update index.jsx

--- site/src/pages/services/assay-lab/index.jsx
+++ site/src/pages/services/assay-lab/index.jsx
@@ -27,13 +27,7 @@ const AssayPage = props => {
       {/* Blocks */}
       <Header content={content['header']} />
       <Divider space='5' />
-      <Container variant='full' sx={styles.heroContainer}>
-        <Hero content={content['hero']} reverse />
-      </Container>
-      <Divider space='5' />
-      <Container variant='wide' sx={styles.contentOneContainer}>
-        <Content content={content['content-one']} />
-      </Container>
+      
       <Table content={content['precious-metals']} />
       <Divider space='5' />
       <Table content={content['precious-metal-assay']} />
