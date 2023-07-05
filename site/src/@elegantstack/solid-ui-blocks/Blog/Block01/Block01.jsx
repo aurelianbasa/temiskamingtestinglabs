be627884d51356b429c9d0f478f5e1e507cbf448
aurelianbasa <56235624+aurelianbasa@users.noreply.github.com>
2023-07-04 21:12:16 -0400

    Update Block01.jsx

--- site/src/@elegantstack/solid-ui-blocks/Blog/Block01/Block01.jsx
+++ site/src/@elegantstack/solid-ui-blocks/Blog/Block01/Block01.jsx
@@ -93,7 +93,7 @@ const BlogBlock01 = ({ content: { text, collection, buttons } }) => (
           {collection.map(
             ({ container, text, images, avatar, buttons }, index) => (
               <Box key={`item-${index}`} sx={{
-                flexBasis: `1/2`,
+                flexBasis: `1/3`,
                 flexGrow: 2,
                 p: 3
               }}>
